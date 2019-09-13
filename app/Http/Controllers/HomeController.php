c7113b9ec8bee6fb0e478d298d0cb7c47927bb73
juwiati547@gmail.com <juwiati547@gamil.com>
2019-09-13 18:33:10 +0800

    tugas laravel 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,48 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    public function myCaptcha()
+    {
+        return view('myCaptcha');
+    }
+
+
+    public function myCaptchaPost(Request $request)
+    {
+
+        request()->validate([
+
+            'email' => 'required|email',
+
+            'password' => 'required',
+
+            'captcha' => 'required|captcha'
+
+        ],
+
+        ['captcha.captcha'=>'Invalid captcha code.']);
+
+        dd("You are here :) .");
+
+    }
+
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
+    public function refreshCaptcha()
+
+    {
+
+        return response()->json(['captcha'=> captcha_img()]);
+
+    }
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,21 @@
                             </div>
                         </div>
 
+                        
+                        <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('phone number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -43,7 +58,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -56,8 +71,8 @@
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                             <div class="col-md-6">
+                                <input id="password" type="password" class="form-control name="password" required autocomplete="new-password">
                             </div>
                         </div>
 
@@ -75,3 +90,4 @@
     </div>
 </div>
 @endsection
+   
